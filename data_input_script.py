7111a47966172e4ffb4ad71fc3aa2eb7b5f04041
Jeffrey Esedo <jiesedo@gmail.com>
2023-07-10 21:49:12 +0100

    code update: the loop for the data

--- data_input_script.py
+++ data_input_script.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 def send_user_data_to_s3(user_data, s3_bucket_url):
-    ApiKey= os.getenv("POSTMAN_API_KEY")
+    api_key= os.getenv("POSTMAN_API_KEY")
 
     if api_key is None:
         print('Postman API key not found. Make sure you have set the environment variable.')
@@ -14,7 +14,7 @@ def send_user_data_to_s3(user_data, s3_bucket_url):
 
     headers = {
         'Content-Type': 'application/json',
-        'X-Api-Key': ApiKey  # Replace with your Postman API key
+        'X-Api-Key': api_key  # Replace with your Postman API key
     }
     
     response = requests.post(s3_bucket_url, json=user_data, headers=headers)
@@ -26,29 +26,33 @@ def send_user_data_to_s3(user_data, s3_bucket_url):
 
 
 # Load Excel workbook
-workbook = openpyxl.load_workbook('Data\MOCK_DATA.xlsx')  
+workbook = openpyxl.load_workbook('DATA/MOCK_DATA.xlsx')  
 
 # Select the desired sheet by name or index
-sheet = workbook['MOCK-DATA']  
+sheet = workbook['MOCK_DATA']  
 
 # Data
-# user_data = data
+user_data={}
+keys=[]
+value=[]
 
 # Iterate through each row in the sheet
+# for col in sheet.iter_cols(values_only=True):
+#     # Access the cell values in each row
+#     for cell_value in col:
+#         user_data.append({col[0]:cell_value})
+
 for col in sheet.iter_cols(values_only=True):
-    # Access the cell values in each row
-    for cell_value in col:
-        user_data={col[0]:cell_value}
+    keys.append(col[0])
+    value.append(col[1:])
 
 # Close the workbook
 workbook.close()
 
 # Enter S3 bucket URL/filename
-s3url= os.getenv("S3_URL")
+s3_bucket_url = s3_url= os.getenv("S3_URL")
 
-if s3url is None:
+if s3_bucket_url is None:
     print('Postman API key not found. Make sure you have set the environment variable.')
-
-s3_bucket_url = s3url
 
 send_user_data_to_s3(user_data, s3_bucket_url)
